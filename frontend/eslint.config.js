d5a546fc1f1cfa7be1e186b7a13cefa733b9527d
estib <stron@me.com>
2024-07-30 12:01:22 +0200

    Scaffold the frontend
    Configure Eslint and Typescript.

---
+++ frontend/eslint.config.js
@@ -0,0 +1,16 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {rules: {
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off"
+  }}
+];
