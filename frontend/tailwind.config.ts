809e7e11de13f81032a92d1eb7cdb127f1e5c627
estib <stron@me.com>
2024-08-02 13:53:40 +0200

    Frontend: Tailwind - Add appear up animation

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -70,10 +70,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "appear-up": {
+          from: { opacity: "0", transform: "translateY(10px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "appear-up": "appear-up 0.2s ease-out",
       },
     },
   },
