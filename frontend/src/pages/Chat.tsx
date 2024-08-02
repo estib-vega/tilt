2bf296644067932f9e41e482b1107b29a13e5050
estib <stron@me.com>
2024-08-02 16:26:48 +0200

    Frontend: Extract the chat page to a dedicated module
    Create directory of pages to extract the main components to them

---
+++ frontend/src/pages/Chat.tsx
@@ -0,0 +1,25 @@
+import ChatInput from "@/components/chat/ChatInput";
+import ChatContent from "@/components/chat/ChatContent";
+import { useChat } from "@/components/hooks/chat";
+
+export default function Chat() {
+  const chat = useChat();
+
+  return (
+    <div className="h-full flex flex-col flex-grow-0 overflow-hidden">
+      <div className="container flex-grow overflow-hidden pt-2">
+        <ChatContent
+          streamingMessage={chat.streamingMessage}
+          messages={chat.messages}
+          isLoadingAnswer={chat.isLoadingAnswer}
+        />
+      </div>
+      <div className="container flex-shrink-0">
+        <ChatInput
+          disabled={chat.isLoadingAnswer}
+          onEnterInput={chat.onEnterInput}
+        />
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes/chat.lazy.tsx
+++ frontend/src/routes/chat.lazy.tsx
@@ -1,30 +1,6 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import ChatInput from "@/components/chat/ChatInput";
-import ChatContent from "@/components/chat/ChatContent";
-import { useChat } from "@/components/hooks/chat";
+import Chat from "@/pages/Chat";
 
 export const Route = createLazyFileRoute("/chat")({
   component: Chat,
 });
-
-function Chat() {
-  const chat = useChat();
-
-  return (
-    <div className="h-full flex flex-col flex-grow-0 overflow-hidden">
-      <div className="container flex-grow overflow-hidden pt-2">
-        <ChatContent
-          streamingMessage={chat.streamingMessage}
-          messages={chat.messages}
-          isLoadingAnswer={chat.isLoadingAnswer}
-        />
-      </div>
-      <div className="container flex-shrink-0">
-        <ChatInput
-          disabled={chat.isLoadingAnswer}
-          onEnterInput={chat.onEnterInput}
-        />
-      </div>
-    </div>
-  );
-}
