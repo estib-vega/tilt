404a097ef24b979485da729cac7deb96d87a41a0
estib <stron@me.com>
2024-08-02 12:28:07 +0200

    Frontend: Add Tanstack Query deps
    Add the dependencies for query handling

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,12 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default [
   { files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
+  ...pluginQuery.configs["flat/recommended"],
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
