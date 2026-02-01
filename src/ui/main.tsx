55b4d906d5fab49e43d4fa16ddd41ddb8b20228b
estib <stron@me.com>
2026-02-01 17:44:13 +0100

    initialize the history on the root
    This fixes a condition in which the electron application wouldn't load
    the root properly. Now it does!

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { RouterProvider, createMemoryHistory, createRouter } from '@tanstack/react-router';
 
 import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx';
 
@@ -13,8 +13,14 @@ import reportWebVitals from './reportWebVitals.ts';
 // Create a new router instance
 
 const TanStackQueryProviderContext = TanStackQueryProvider.getContext();
+
+const memoryHistory = createMemoryHistory({
+  initialEntries: ['/'],
+});
+
 const router = createRouter({
   routeTree,
+  history: memoryHistory,
   context: {
     ...TanStackQueryProviderContext,
   },
